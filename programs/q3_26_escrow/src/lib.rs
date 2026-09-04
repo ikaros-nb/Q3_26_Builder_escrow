1e469cd0fe452bcffe4e33f3e925f805395aa9c0
Nicolas Bouème <nicolasboueme@gmail.com>
2026-09-04 14:12:10 +0900

    feat(make): implement instruction

--- programs/q3_26_escrow/src/lib.rs
+++ programs/q3_26_escrow/src/lib.rs
@@ -15,8 +15,14 @@ declare_id!("F7rM6r4imQypkGyrRnukZfB1iv8YHkPYKUzDSGE85U7A");
 pub mod q3_26_escrow {
     use super::*;
 
-    pub fn make(ctx: Context<Make>) -> Result<()> {
-        Ok(())
+    pub fn make(
+        ctx: Context<Make>,
+        id: u64,
+        deposit: u64,
+        mint_b_wanted_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts.populate_escrow(id, mint_b_wanted_amount, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
     }
 
     pub fn take(ctx: Context<Take>) -> Result<()> {
